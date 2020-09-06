89d470f7dffa8d53f1f33e57157308a1a2fde37d
kuszner <kuszner@gmail.com>
2020-09-06 12:01:27 +0200

    Test

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "wabbit-dev"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "aCG-tests"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
